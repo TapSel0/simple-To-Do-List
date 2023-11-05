d84f26dcbc30b050a34907b736999ca5e4e0dc43
TapSel0 <79279863+TapSel0@users.noreply.github.com>
2023-11-05 16:27:11 +0700

    Change "complete" to "done"

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ root.title("ToDoList")  # название окна
 
 todo_list = Listbox(root, height=10, width=30)  # колоночка для запланированных дел
 in_progress_list = Listbox(root, height=10, width=30)  # колонка для дел в процессе
-complete_list = Listbox(root, height=10, width=30)  # колонка для выполненных дел
+done_list = Listbox(root, height=10, width=30)  # колонка для выполненных дел
 
 todo_list.grid(row=0, column=0, padx=5, pady=5)  # размещение по сетке с добавлением отступов
 in_progress_list.grid(row=0, column=1, padx=5, pady=5)
-complete_list.grid(row=0, column=2, padx=5, pady=5)
+done_list.grid(row=0, column=2, padx=5, pady=5)
 
 label_add_task = Label(root, text="Add task: ")  # надпись
 label_add_task.grid(row=1, column=0, pady=5)  # размещение надписи
